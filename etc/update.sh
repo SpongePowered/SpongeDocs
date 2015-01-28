f37d72cb191c76bd0e2b5982685150dbff0cceb2
gratimax <max@ovsankin.com>
2015-01-28 09:36:05 -0800

    Add configuration for updating Transifex strings using Travis

---
+++ etc/update.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+LANGUAGES=$(curl --user $TX_USER:$TX_PASS http://transifex.com/api/2/project/sponge-docs/languages/ | jq -r '.[] | .language_code | "-l " + .' | xargs echo)
+
+echo languages: $LANGUAGES
+
+echo | tx init --user=$TX_USER --pass=$TX_PASS
+
+make gettext
+sphinx-intl update -p build/locale $LANGUAGES -d locale/ > /dev/null/
+
+sphinx-intl update-txconfig-resources -p build/locale --transifex-project-name sponge-docs -d locale/
+
+tx push -s
