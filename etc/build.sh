260a8b3d42da1ce6357d97da2ba1ad09ca4b7427
gratimax <max@ovsankin.com>
2015-01-29 00:31:40 -0800

    Build prs and error with warnings

---
+++ etc/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+red=`tput setaf 1`
+bold=`tput bold`
+reset=`tput sgr0`
+sphinx-build -b html source build/html 2> warnings
+if [[ -n $(cat warnings) ]]; then
+  echo ${red}${bold}
+  echo "WARNINGS(These are causing your build to fail):"
+  echo "==============================================="
+  cat warnings
+  exit 1
+fi
