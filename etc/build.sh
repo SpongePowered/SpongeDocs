9094c4e7cd87054ce9c07f92d28ece47d476a8ed
gratimax <max@ovsankin.com>
2015-01-29 15:03:08 -0800

    Unify build scripts

--- etc/build.sh
+++ etc/build.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env bash
+
 sphinx-build -b html source build/html 2> errors
 if [[ -n $(cat errors) ]]; then
   python ./etc/reporter.py fail
   exit 1
 else
   python ./etc/reporter.py pass
-  exit 0
 fi
+
+if [[ $TRAVIS_PULL_REQUEST = false && $TRAVIS_BRANCH = master ]]; then
+
+  LANGUAGES=$(curl --user $TX_USER:$TX_PASS http://transifex.com/api/2/project/sponge-docs/languages/ | jq -r '.[] | .language_code | "-l " + .' | xargs echo)
+
+  echo languages: $LANGUAGES
+
+  echo | tx init --user=$TX_USER --pass=$TX_PASS
+
+  sphinx-build -b gettext source build/locale
+  sphinx-intl update -p build/locale $LANGUAGES -d locale/ > /dev/null
+
+  sphinx-intl update-txconfig-resources -p build/locale --transifex-project-name sponge-docs -d locale/
+
+  tx push -s
+
+fi
+
+exit 0
--- etc/script.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-./etc/build.sh
-result=$?
-if [[ $TRAVIS_PULL_REQUEST = false && $TRAVIS_BRANCH = master ]]; then
-  ./etc/update.sh
-fi
-exit $?
