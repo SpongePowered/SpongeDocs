fd7a2871173311d9e2a175bdb5076dd59cdfa0bb
gratimax <max@ovsankin.com>
2015-01-29 01:04:38 -0800

    Use conventional colorful warnings

--- etc/build.sh
+++ etc/build.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
-red=`tput setaf 1`
-bold=`tput bold`
-reset=`tput sgr0`
+red="\e[1;31m"
+green="\e[1;32m"
+bold=""
+reset="\e[0m"
 sphinx-build -b html source build/html 2> warnings
 if [[ -n $(cat warnings) ]]; then
-  echo ${red}${bold}
+  echo -e ${red}
   echo "WARNINGS (These are causing your build to fail):"
   echo "================================================"
   cat warnings
   echo "END WARNINGS"
   echo "============"
-  echo ${reset}
+  echo -e ${reset}
   exit 1
+else
+  echo -e ${green}
+  echo "NO WARNINGS (Good job!)"
+  echo "======================="
+  echo -e ${reset}
+  exit 0
 fi
