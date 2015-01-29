3bfeb40255100cebce74f03cf23462edd9214aed
gratimax <max@ovsankin.com>
2015-01-29 00:42:05 -0800

    Add a few fixes and changes

--- etc/build.sh
+++ etc/build.sh
@@ -5,8 +5,8 @@ reset=`tput sgr0`
 sphinx-build -b html source build/html 2> warnings
 if [[ -n $(cat warnings) ]]; then
   echo ${red}${bold}
-  echo "WARNINGS(These are causing your build to fail):"
-  echo "==============================================="
+  echo "WARNINGS (These are causing your build to fail):"
+  echo "================================================"
   cat warnings
   exit 1
 fi
