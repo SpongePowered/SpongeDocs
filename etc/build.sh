c91a7551c9d0a3b70a942c555a829fe5a39a19b2
gratimax <max@ovsankin.com>
2015-02-22 13:09:40 -0800

    Deploy translated docs

--- etc/build.sh
+++ etc/build.sh
@@ -1,21 +1,33 @@
 #!/usr/bin/env bash
 
+# Attempt to build the docs
 sphinx-build -b html -d build/doctrees source build/html 2> errors
+
+# Fail if there are errors
 if [[ -n $(cat errors) ]]; then
-  python ./etc/reporter.py fail
-  exit 1
+    python ./etc/reporter.py fail
+    exit 1
 else
-  python ./etc/reporter.py pass
+    python ./etc/reporter.py pass
 fi
 
+# If we're on the master branch, do deploys
 if [[ $TRAVIS_PULL_REQUEST = false && $TRAVIS_BRANCH = master ]]; then
 
-  echo -e \\napi_key: ${CROWDIN_API_KEY} >> ./crowdin.yaml
+    # Add the api key to the crowdin configuration (because it is stupid)
+    echo -e \\napi_key: ${CROWDIN_API_KEY} >> ./crowdin.yaml
 
-  sphinx-build -b gettext source build/locale
-  sphinx-intl update -p build/locale -l en -d locale-src
+    # Build the *.pot files
+    sphinx-build -b gettext source build/locale
 
-  crowdin-cli upload sources
+    # Make them #.po files (because crowdin is stupid)
+    sphinx-intl update -p build/locale -l en -d locale-src
+
+    # Upload the sources
+    crowdin-cli upload sources
+
+    # Deploy docs
+    ./etc/docs.sh
 
 fi
 
---
+++ etc/docs.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# Obtain the list of crowdin supported languages
+curl https://api.crowdin.com/api/supported-languages?json > langs.json
+
+# Build sources for crowdin
+sphinx-build -b gettext source build/locale
+sphinx-intl update -p build/locale -l en -d locale-src 1>/dev/null
+
+# Download translations
+crowdin-cli download
+
+# Build translated .po files
+sphinx-intl build -d locale/ 1>/dev/null
+
+# Build the english source
+sphinx-build -b html source dist/en
+
+# We need the platform for xargs args
+platform='unknown'
+unamestr=$(uname)
+
+# Build each language
+if [[ "$unamestr" == 'FreeBSD' || "$unamestr" == 'Darwin'  ]]; then
+    find locale -d 1 |
+    sed 's/locale\///' |
+    xargs -L 1 ./etc/sphinx_opts.sh |
+    xargs -P 4 -I % sh -c "sphinx-build -b html %"
+else
+    find locale -d 1 |
+    sed 's/locale\///' |
+    xargs -L 1 ./etc/sphinx_opts.sh |
+    xargs --max-procs=4 -I % sh -c "sphinx-build -b html %"
+fi
+
+# Build the homepage
+python ./etc/home.py $(find locale -d 1 |
+sed 's/locale\///' |
+tr '\n' ',' |
+sed 's/,$//g')
+
+# No jekyll !!!
+touch dist/.nojekyll
+
+# Deploy
+cd dist
+git config --global user.name "Spongy"
+git config --global user.email "spongy@gratimax.net"
+git init
+git remote add origin https://spongy:${GH_TOKEN}@github.com/Spongy/SpongeDocs >/dev/null
+git checkout -b gh-pages
+git add .
+git commit -m "Deploy $(date)"
+git push -f origin gh-pages >/dev/null
