97f5c933879f041ab3923306ce82befc78885e02
gratimax <max@ovsankin.com>
2015-02-22 13:32:02 -0800

    Add back sphinx_opts too

---
+++ etc/sphinx_opts.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+locale=$(echo $1 | sed s/_/-/)
+crowdin_code=$(cat langs.json | jq  -r --arg lo "$locale" 'map(select(.locale == $lo)) | .[:1] | .[] | .crowdin_code')
+echo "-D language=$1 source dist/$crowdin_code"
--- etc/test.sh
+++
@@ -1,4 +0,0 @@
-sphinx-intl build -d locale/ 1>/dev/null
-find locale -d 1 | 
-sed 's/locale\///' | 
-xargs -P 16 -I % sphinx-build -b html -D language=% source build/lang/%
