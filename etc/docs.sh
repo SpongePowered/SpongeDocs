ce06b49dd9dc2468166054b478413d33e894d94a
gratimax <max@ovsankin.com>
2015-02-23 10:05:38 -0800

    Remove unecessary stuff, silence git

--- etc/docs.sh
+++ etc/docs.sh
@@ -57,5 +57,5 @@ git init
 git remote add origin https://spongy:${GH_TOKEN}@github.com/Spongy/SpongeDocs >/dev/null
 git checkout -b gh-pages
 git add .
-git commit -m "Deploy $(date)" >/dev/null
-git push -f origin gh-pages >/dev/null
+git commit -q -m "Deploy $(date)" >/dev/null
+git push -q -f origin gh-pages >/dev/null
