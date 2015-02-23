7c89552ba2f54cc8980205390389b9d5186f81bb
gratimax <max@ovsankin.com>
2015-02-23 10:18:29 -0800

    Remove the _static directory ref

--- etc/docs.sh
+++ etc/docs.sh
@@ -55,7 +55,7 @@ git config --global user.name "Spongy"
 git config --global user.email "spongy@gratimax.net"
 git init
 git remote add origin https://spongy:${GH_TOKEN}@github.com/Spongy/SpongeDocs >/dev/null
-git checkout -b gh-pages
+git checkout --orphan gh-pages
 git add .
 git commit -q -m "Deploy $(date)" >/dev/null
 git push -q -f origin gh-pages >/dev/null
