d7f08b5efd0efe5f6456a8eb3f3ee0cc4761d283
gratimax <max@ovsankin.com>
2015-03-09 00:17:19 -0700

    Add list of modified source files to comment

--- etc/comment.py
+++ etc/comment.py
@@ -5,11 +5,15 @@ import json
 comment_tpl = """
 A preview for this pull request is available at %s/%s/index.html.
 
+Here are some links to the pages that were modified:
+
+%s
+
 _Since the preview always changes, please link to [this comment](%s), not to the direct url to the preview._
 """
 
-def mk_comment(commit, comment):
-    return {'body': comment_tpl % (rawgit, commit, comment)}
+def mk_comment(commit, comment, files):
+    return {'body': comment_tpl % (rawgit, commit, '\n'.join('- %s/%s/%s.html' % (rawgit, commit, file) for file in files), comment)}
 
 pr = os.environ.get('TRAVIS_PULL_REQUEST')
 token = os.environ.get('GH_TOKEN')
@@ -19,6 +23,9 @@ repo = 'https://api.github.com/repos/SpongePowered/SpongeDocs'
 pr_repo = 'https://api.github.com/repos/Spongy/SpongeDocs-PRs'
 
 commit = requests.get('%s/branches/%s' % (pr_repo, pr)).json()['commit']['sha'][:8]
+
+files = [filename[filename.find('/')+1:filename.rfind('.')] for filename in
+         [change['filename'] for change in requests.get('%s/pulls/%s/files' % (repo, pr)).json()] if filename.find('source/') == 0]
 
 comments = requests.get('%s/issues/%s/comments' % (repo, pr), auth=('x-oauth-basic', token)).json()
 spongy_comments = [comment for comment in comments if comment['user']['login'] == 'Spongy']
@@ -30,7 +37,7 @@ if spongy_comments:
     requests.patch(
         '%s/issues/comments/%s' % (repo, comment_id),
         auth=('x-oauth-basic', token),
-        data=json.dumps(mk_comment(commit, comment_url)))
+        data=json.dumps(mk_comment(commit, comment_url, files)))
 else:
     payload = {'body': 'Setting up PR reference, please wait...'}
     comment = requests.post(
@@ -42,4 +49,4 @@ else:
     requests.patch(
         '%s/issues/comments/%s' % (repo, comment_id),
         auth=('x-oauth-basic', token),
-        data=json.dumps(mk_comment(commit, comment_url)))
+        data=json.dumps(mk_comment(commit, comment_url, files)))
