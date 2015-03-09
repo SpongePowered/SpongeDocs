1973c6fba31328f8755961de82115a9d31f210ab
gratimax <max@ovsankin.com>
2015-03-09 08:38:52 -0700

    Fix behavior if remote PR branch does not exist

--- etc/pr.sh
+++ etc/pr.sh
@@ -6,12 +6,12 @@ branch_exists=$(git show-ref refs/heads/${branch})
 
 # Deploy
 cd build/html
-git config --global user.name "Spongy"
-git config --global user.email "spongy@gratimax.net"
+git config user.name "Spongy"
+git config user.email "spongy@gratimax.net"
 git init
 git remote add origin https://spongy:${GH_TOKEN}@github.com/Spongy/SpongeDocs-PRs >/dev/null
-git fetch origin $branch
 if [[ -n "$branch_exists" ]]; then
+    git fetch origin $branch
     git checkout $branch
 else
     git checkout --orphan $branch
