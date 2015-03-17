85f91bc333fc5171d179e139973b55e3f6468220
gratimax <max@ovsankin.com>
2015-03-16 20:40:39 -0700

    Whoops 3: The Search for the Whoopseler

--- etc/install.sh
+++ etc/install.sh
@@ -5,13 +5,3 @@ gem install crowdin-cli
 
 # Install pip requirements
 pip install -r etc/requirements.txt
-
-# Install spongedocs-theme dependencies
-cd spongedocs-theme/
-
-# install node modules
-npm install
-
-# install bower components
-npm install bower
-./node_modules/.bin/bower install
