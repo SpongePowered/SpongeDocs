81a669b09a71b61c606a6ab11d2e3de46ca12408
Jamie Mansfield <dev@jamierocks.uk>
2016-09-07 20:21:30 +0100

    Neaten Gulp scripts and update deps

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,34 +1,31 @@
-var gulp = require('gulp');
-
-var webserver = require('gulp-webserver');
-var shell = require('gulp-shell');
-
-var del = require('del');
-
+var gulp = require('gulp'),
+    shell = require('gulp-shell'),
+    webserver = require('gulp-webserver'),
+    del = require('del');
 
 var target = 'dirhtml';
 
 gulp.task('clean', function (cb) {
-  del([
-    'build'
-  ], cb);
+    del([
+        'build'
+    ], cb);
 });
 
 gulp.task('sphinx', shell.task(
-  'sphinx-build -b ' + target + ' source build/' + target
+    'sphinx-build -b ' + target + ' source build/' + target
 ));
 
 gulp.task('webserver', ['sphinx'], function () {
-  gulp.src('build/' + target)
-    .pipe(webserver({
-      livereload: true,
-      enable: true,
-      open: true
-    }));
+    gulp.src('build/' + target)
+        .pipe(webserver({
+            livereload: true,
+            enable: true,
+            open: true
+        }));
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['source/**/*'], ['sphinx']);
+    gulp.watch(['source/**/*'], ['sphinx']);
 });
 
 gulp.task('default', ['clean', 'sphinx', 'webserver', 'watch']);
