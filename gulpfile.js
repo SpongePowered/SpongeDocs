e93604a804e265491328dcbd75526096a3c67141
Minecrell <dev@minecrell.net>
2017-01-22 19:14:43 +0100

    Switch to new theme based on sphinx_rtd_theme

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,51 @@
-var gulp = require('gulp'),
-    shell = require('gulp-shell'),
-    webserver = require('gulp-webserver'),
-    del = require('del');
+const
+    gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    del = require('del'),
+    spawn = require('child_process').spawn,
 
-var target = 'dirhtml';
+    webserver = require('gulp-webserver');
 
-gulp.task('clean', function (cb) {
-    del([
-        'build'
-    ], cb);
-});
+gulp.task('clean', () => del(['build']));
 
-gulp.task('sphinx', shell.task(
-    'sphinx-build -b ' + target + ' source build/' + target
+function shell(plugin, command, args) {
+    return (done) =>
+        spawn(command, args, {stdio: 'inherit'})
+            .on('error', (err) => {
+                done(new gutil.PluginError(plugin, err))
+            })
+            .on('exit', (code) => {
+                if (code == 0) {
+                    // Process completed successfully
+                    done()
+                } else {
+                    done(new gutil.PluginError(plugin, `Process failed with exit code ${code}`));
+                }
+            })
+}
+
+gulp.task('sphinx', shell(
+    'sphinx', 'sphinx-build', ['-W', '-d', 'build/doctrees', 'source', 'build/html']
 ));
 
-gulp.task('webserver', ['sphinx'], function () {
-    gulp.src('build/' + target)
+gulp.task('sphinx:dev', shell(
+    'sphinx', 'sphinx-build', ['source', 'build/dev/html']
+));
+
+gulp.task('build', ['clean', 'sphinx']);
+
+// TODO: Don't stop watching if Sphinx or Sass throws an error
+gulp.task('watch', ['clean', 'sphinx:dev'], () =>
+    gulp.watch('./source/**', ['sphinx:dev'])
+);
+
+gulp.task('webserver', ['watch'], () =>
+    gulp.src('build/dev/html')
         .pipe(webserver({
             livereload: true,
             enable: true,
             open: true
-        }));
-});
+        }))
+);
 
-gulp.task('watch', function () {
-    gulp.watch(['source/**/*'], ['sphinx']);
-});
-
-gulp.task('default', ['clean', 'sphinx', 'webserver', 'watch']);
+gulp.task('default', ['webserver']);
