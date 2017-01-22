87e7107b3723fadb609f6c8492df6f8706a7ba96
Minecrell <dev@minecrell.net>
2017-01-22 19:14:43 +0100

    Switch to new theme based on sphinx_rtd_theme

--- etc/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Install crowdin
-gem install crowdin-cli
-
-# Install pip requirements
-pip install -r etc/requirements.txt
--- etc/sphinx_opts.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-locale=$(echo $1 | sed s/_/-/)
-crowdin_code=$(cat langs.json | jq  -r --arg lo "$locale" 'map(select(.locale == $lo)) | .[:1] | .[] | .crowdin_code')
-echo "-D language=$1 source dist/$BRANCHNAME/$crowdin_code"
