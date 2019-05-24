762efc0e24faebb333fec75afd1c345a2ce46bef
phit <phit@hush.com>
2019-05-24 21:07:23 +0200

    start working on upgrading all the deps and get a clean npm audit

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,29 @@
-const
-    gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    del = require('del'),
-    spawn = require('child_process').spawn,
+'use strict';
 
-    webserver = require('gulp-webserver');
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var shell = require('gulp-shell');
+var del = require('del');
 
-gulp.task('clean', () => del(['build']));
+gulp.task('clean', del.bind(null, ['build']));
 
-function shell(plugin, command, args) {
-    return (done) =>
-        spawn(command, args, {stdio: 'inherit'})
-            .on('error', (err) => {
-                done(new gutil.PluginError(plugin, err))
-            })
-            .on('exit', (code) => {
-                if (code == 0) {
-                    // Process completed successfully
-                    done()
-                } else {
-                    done(new gutil.PluginError(plugin, `Process failed with exit code ${code}`));
-                }
-            })
-}
-
-gulp.task('sphinx', shell(
-    'sphinx', 'sphinx-build', ['-W', '-d', 'build/doctrees', 'source', 'build/html']
-));
-
-gulp.task('sphinx:dev', shell(
-    'sphinx', 'sphinx-build', ['source', 'build/dev/html']
-));
+gulp.task('sphinx', shell.task('sphinx-build -W -d build/doctrees source build/html'));
+gulp.task('sphinx:dev', shell.task('sphinx-build source build/dev/html'));
 
 gulp.task('build', gulp.series('clean', 'sphinx'));
 gulp.task('build:dev', gulp.series('clean', 'sphinx:dev'));
 
+gulp.task('connect', function(done) {
+  connect.server({
+    root: 'build/dev/html',
+    livereload: true
+  });
+  done();
+});
+
 // TODO: Don't stop watching if Sphinx or Sass throws an error
-gulp.task('watch', () =>
-    gulp.watch('./source/**', gulp.series('sphinx:dev'))
-);
+gulp.task('watch', () => {
+  gulp.watch('./source/**', gulp.series('sphinx:dev'));
+});
 
-gulp.task('webserver', gulp.series('build:dev', gulp.parallel('watch', () =>
-    gulp.src('build/dev/html')
-        .pipe(webserver({
-            livereload: true,
-            enable: true,
-            open: true
-        }))
-)));
-
-gulp.task('default', gulp.series('webserver'));
+gulp.task('default', gulp.series('build:dev', gulp.parallel('connect', 'watch')));
