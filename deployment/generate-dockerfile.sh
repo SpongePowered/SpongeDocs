9af21d2a25096667298e9d125ac5d0895e52cf07
Felix Kleinekathoefer <git@felixoi.com>
2020-11-10 20:20:30 +0100

    Try this differently

--- deployment/generate-dockerfile.sh
+++ deployment/generate-dockerfile.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env sh
 
-versions=$(list-versions)
-
 touch deployment/Dockerfile
 
 echo "FROM ghcr.io/felixoi/sponge-docs-theme:latest as homepage" >> deployment/Dockerfile
 i=0
-for version in $versions; do
+for version in $VERSIONS; do
     echo "FROM ghcr.io/felixoi/spongedocs:$version as builder-$i" >> deployment/Dockerfile
     i=$(( i + 1 ))
 done
@@ -15,7 +13,7 @@ echo "FROM nginx:1.19.4-alpine" >> deployment/Dockerfile
 echo "COPY --from=homepage /usr/share/nginx/html /usr/share/nginx/html" >> deployment/Dockerfile
 
 i=0
-for version in $versions; do
+for version in $VERSIONS; do
     echo "COPY --from=builder-$i /usr/share/nginx/html/$version /usr/share/nginx/html/$version" >> deployment/Dockerfile
     i=$(( i + 1 ))
 done
