47e1979d5349f08f0ed51d28a338259235c215f2
ST-DDT <ST-DDT@gmx.de>
2021-12-31 00:39:11 +0100

    Update configurate javadoc package config

--- source/conf.py
+++ source/conf.py
@@ -96,7 +96,7 @@ gettext_compact = False
 
 javadoc_links = {
     "https://jd.spongepowered.org/%s/" % release: ["org.spongepowered.api"],
-    "https://configurate.aoeu.xyz/apidocs/": ["ninja.leaping.configurate"],
+    "https://configurate.aoeu.xyz/apidocs/": ["org.spongepowered.configurate"],
     "https://docs.oracle.com/javase/8/docs/api/": ["java"],
     "https://google.github.io/guava/releases/17.0/api/docs/": ["com.google.common"],
     "https://jd.adventure.kyori.net/api/4.7.0/": ["net.kyori.adventure"],
